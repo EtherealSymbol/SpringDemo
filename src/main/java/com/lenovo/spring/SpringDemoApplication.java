97eafb69743e0a5f51f607abcd456363a7f64109
EtherealSymbol <2391892926@qq.com>
2021-07-23 17:31:21 +0800

    Update SpringDemoApplication.java

--- src/main/java/com/lenovo/spring/SpringDemoApplication.java
+++ src/main/java/com/lenovo/spring/SpringDemoApplication.java
@@ -2,8 +2,9 @@ package com.lenovo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //排除自动配置
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
