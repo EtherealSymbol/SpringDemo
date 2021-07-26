696440dec89d6c60602b0fdd90da2342f08c272a
EtherealSymbol <2391892926@qq.com>
2021-07-26 15:16:17 +0800

    Update SpringDemoApplicationTests.java

--- src/test/java/com/lenovo/spring/SpringDemoApplicationTests.java
+++ src/test/java/com/lenovo/spring/SpringDemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.lenovo.spring;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringDemoApplicationTests {
+	
+	@Before
+    public void init() {
+        System.out.println("开始测试-----------------");
+    }
 
 	@Test
 	void contextLoads() {
 	}
+	
+	@After
+    public void after() {
+        System.out.println("测试结束-----------------");
+    }
 
 }
