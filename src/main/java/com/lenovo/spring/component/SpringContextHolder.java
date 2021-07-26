b74ae9b2a47f865232a4140ac9b4e3c120c37e52
EtherealSymbol <2391892926@qq.com>
2021-07-26 15:14:24 +0800

    Create SpringContextHolder.java

---
+++ src/main/java/com/lenovo/spring/component/SpringContextHolder.java
@@ -0,0 +1,57 @@
+package com.lenovo.spring.component;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/*
+ * 通过freemarker生成静态html
+ * 1.需要编写一个可以在普通类中获取到springbean的工具类SpringContextHolder
+ * */
+@Component
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext appContext = null;
+
+    /**
+     * 通过name获取 Bean.
+     *
+     * @param name
+     * @return
+     */
+    public static Object getBean(String name) {
+        return appContext.getBean(name);
+
+    }
+
+    /**
+     * 通过class获取Bean.
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return appContext.getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return appContext.getBean(name, clazz);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (appContext == null) {
+            appContext = applicationContext;
+        }
+    }
+}
