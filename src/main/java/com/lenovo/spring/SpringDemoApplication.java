93f468be859908fd8656955b6233d4f1f90952f6
EtherealSymbol <2391892926@qq.com>
2021-07-27 17:31:34 +0800

    Update SpringDemoApplication.java

--- src/main/java/com/lenovo/spring/SpringDemoApplication.java
+++ src/main/java/com/lenovo/spring/SpringDemoApplication.java
@@ -1,10 +1,13 @@
 package com.lenovo.spring;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //排除自动配置
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //排除自动配置
+@SpringBootApplication
+@MapperScan("com.lenovo.spring.dao")
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
